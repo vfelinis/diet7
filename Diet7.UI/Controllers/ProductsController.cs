3337b997ab475d6fd96e109c957e5e9862518d57
ponomarev_mike <ponomarev.mike@sharp-dev.net>
2023-09-16 21:52:04 +0400

    second

--- Diet7.UI/Controllers/ProductsController.cs
+++ Diet7.UI/Controllers/ProductsController.cs
@@ -59,9 +59,6 @@ namespace Diet7.UI.Controllers
         {
             if (ModelState.IsValid)
             {
-
-                if ()
-
                 _context.Add(product);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
